adffe46d94860be19da3e67540924dd7b21c0b31
Li Dai <li.lea.dai@gmail.com>
2020-12-15 14:55:27 -0800

    tests passing using simulation

--- lib/diagonal_traversal.rb
+++ lib/diagonal_traversal.rb
@@ -10,5 +10,35 @@
 # Example Output:  [1, 2, 4, 7, 5, 3, 6, 8, 9]
 
 def diagonal_traversal(matrix)
-  raise NotImplementedError, "This method has not yet been implemented"
+  r = 0
+  c = 0
+  rows = matrix.length
+  cols = matrix[0].length
+  result = []
+  while  c < cols && r < rows
+    while r >= 0 && c < cols
+      result << matrix[r][c]
+      return result if r == rows - 1 && c == cols - 1
+      c += 1
+      r -= 1
+    end
+    if c >= cols
+      c -= 1
+      r += 2
+    end
+    r += 1 if r < 0
+    while c >= 0 && r < rows
+      result << matrix[r][c]
+      return result if r == rows - 1 && c == cols - 1
+      c -= 1
+      r += 1
+    end
+    if r >= rows
+      r -= 1
+      c += 2
+    end
+    c += 1 if c < 0
+  end
+  return []
 end
+
