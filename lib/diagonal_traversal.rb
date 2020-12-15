0c532818dfb69c3193b327b0b7e83ac7cf5250fb
Li Dai <li.lea.dai@gmail.com>
2020-12-15 15:05:38 -0800

    added comments

--- lib/diagonal_traversal.rb
+++ lib/diagonal_traversal.rb
@@ -16,28 +16,29 @@ def diagonal_traversal(matrix)
   cols = matrix[0].length
   result = []
   while  c < cols && r < rows
-    while r >= 0 && c < cols
+    while r >= 0 && c < cols  #keep going up diagonally until out of bounds
       result << matrix[r][c]
       return result if r == rows - 1 && c == cols - 1
       c += 1
       r -= 1
     end
-    if c >= cols
+    if c >= cols #reverse step and move down when hitting the right boundary
       c -= 1
       r += 2
     end
-    r += 1 if r < 0
-    while c >= 0 && r < rows
+    r += 1 if r < 0 #move down if hitting top boundary
+
+    while c >= 0 && r < rows #keep going down diagonally out of bounds
       result << matrix[r][c]
       return result if r == rows - 1 && c == cols - 1
       c -= 1
       r += 1
     end
-    if r >= rows
+    if r >= rows #reverse step and move right when hitting bottom boundary
       r -= 1
       c += 2
     end
-    c += 1 if c < 0
+    c += 1 if c < 0 #move right when hitting left boundary
   end
   return []
 end
