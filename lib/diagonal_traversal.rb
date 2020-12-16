a715bfd2a4becb6a9ac64b527b1d5e40efd3fd8a
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-12-15 19:32:39 -0800

    added method for diagonal_traversal and passed the tests

--- lib/diagonal_traversal.rb
+++ lib/diagonal_traversal.rb
@@ -10,5 +10,52 @@
 # Example Output:  [1, 2, 4, 7, 5, 3, 6, 8, 9]
 
 def diagonal_traversal(matrix)
-  raise NotImplementedError, "This method has not yet been implemented"
+  return [] if matrix.empty? || (matrix.length == 1 && matrix[0].empty?)
+  runs = (matrix.length - 1) + (matrix[0].length - 1)
+  up = true
+  round = 0
+  row = 0
+  col = 0
+  result = [matrix[row][col]]
+  while (row + col) == round && round <= runs && row >= 0 && col >= 0
+    if up
+      if (row-1) >= 0 && (col+1) >= 0 && !matrix[row].nil? && matrix[row-1][col+1]
+        row -= 1
+        col += 1
+      elsif (row-1) >= 0 && (col+1) >= 0 && matrix[row].nil? && (row + col) != runs
+        row -= 1
+        col += 1
+      else
+        if row == 0
+          col += 1
+          up = false
+          round += 1 
+        else
+          row -= 1
+          col += 1
+        end
+      end
+    else
+      if (row+1) >= 0 && (col-1) >= 0 && !matrix[row+1].nil? && matrix[row+1][col-1]
+        row += 1
+        col -= 1
+      elsif (row+1) >= 0 && (col-1) >= 0 && matrix[row+1].nil? && (row + col) != runs
+        row += 1
+        col -= 1
+      else
+        if col == 0 && (row + col) != runs
+          row += 1
+          up = true
+          round += 1 
+        else
+          row += 1
+          col -= 1
+        end
+      end
+    end
+    if !matrix[row].nil? && !matrix[row][col].nil? 
+      result.push(matrix[row][col]) 
+    end
+  end
+  return result
 end
