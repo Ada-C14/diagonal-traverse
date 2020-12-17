0ddeb6a654c97aec92f49d00eabc06ba0e46b632
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-12-16 16:34:26 -0800

    another thought about looping the diagonal traversal by checking the round num is even or odd, also, passed leetcode time limit exceeded by using a little trick.

--- lib/diagonal_traversal.rb
+++ lib/diagonal_traversal.rb
@@ -1,38 +1,27 @@
 def diagonal_traversal(matrix)
-  return [] if matrix.empty? || (matrix.length == 1 && matrix[0].empty?)
-  runs = (matrix.length - 1) + (matrix[0].length - 1)
-  direction_up = true
+  return [] if matrix.empty?
+  m = matrix.length 
+  n = matrix[0].length
   round = 0
   row = 0
   col = 0
   result = [matrix[row][col]]
-  while round <= runs && row >= 0 && col >= 0
-    if direction_up
+  # to pass leetcode's Time Limit Exceeded, I added this two conditions
+  return matrix[0] if m == 1
+  return result = (0...m).map {|row| matrix[row][0] } if n == 1 
+  
+  while round < (m + n) && row >= 0 && col >= 0
+    if round.even?
       col += 1
-      if (row-1) >= 0 && !matrix[row].nil? && (row + col) != runs
-        row -= 1
-      else
-        if row == 0
-          direction_up = false
-          round += 1 
-        else
-          row -= 1
-        end
-      end
+      round += 1 if row == 0
+      row -= 1 if (row-1) >= 0 
     else
       row += 1
-      if (col-1) >= 0 && !matrix[row].nil? && (row + col) != runs
-        col -= 1
-      else
-        if col == 0 
-          direction_up = true
-          round += 1 
-        else
-          col -= 1
-        end
-      end
+      round += 1 if col == 0 
+      col -= 1 if (col-1) >= 0 
     end
-    result.push(matrix[row][col]) if !matrix[row].nil? && !matrix[row][col].nil? 
+    result.push(matrix[row][col]) unless matrix[row].nil? 
   end
+  result.delete(nil)
   return result
 end
