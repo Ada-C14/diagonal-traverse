d577dc169fb2eae4d4011b518913c5775ad414ce
Christabel Sebastian <christabel.escarez@gmail.com>
2020-12-16 16:00:24 -0800

    who made this monster???

--- lib/diagonal_traversal.rb
+++ lib/diagonal_traversal.rb
@@ -10,5 +10,53 @@
 # Example Output:  [1, 2, 4, 7, 5, 3, 6, 8, 9]
 
 def diagonal_traversal(matrix)
-  raise NotImplementedError, "This method has not yet been implemented"
+  num_rows = matrix.length
+  num_columns = matrix[0].length
+  diagonal = Array.new
+  movement = :up
+  current_row = 0
+  current_column = 0
+
+  (num_rows * num_columns).times do
+    diagonal << matrix[current_row][current_column]
+
+    puts "-----"
+    puts "value: #{matrix[current_row][current_column]} #{movement}"
+    puts "location: [#{current_row}, #{current_column}]"
+    pp current_column == (num_columns - 1)
+
+    if movement == :up
+
+      if current_row < 1 || (current_column == num_columns - 1)
+        movement = :down
+        if current_column == (num_columns - 1)
+          current_row += 1
+        else
+          current_column += 1
+        end
+      else
+        current_row -= 1
+        current_column += 1
+      end
+
+    elsif movement == :down
+
+      if current_column < 1 || current_row == (num_rows - 1)
+        movement = :up
+        if current_row == (num_rows - 1)
+          current_column += 1
+        else
+          current_row += 1
+        end
+      else
+        current_row += 1
+        current_column -= 1
+      end
+
+
+    end
+
+  end
+
+  return diagonal
 end
