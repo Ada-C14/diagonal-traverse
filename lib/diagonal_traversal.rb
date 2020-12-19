9eec11a052a0a04a1ab839bfbd004bc609781032
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-12-18 20:00:33 -0800

    implement diagonal_traversal

--- lib/diagonal_traversal.rb
+++ lib/diagonal_traversal.rb
@@ -3,12 +3,34 @@
 # Return value:  All elements of the matrix in diagonal order.
 #  Example input:
 # [
-#  [ 1, 2, 3 ],
-#  [ 4, 5, 6 ],
-#  [ 7, 8, 9 ]
+#  [ 1, 2, 3 ], -->  [0][0], [0][1], [0][2]
+#  [ 4, 5, 6 ], -->  [1][0], [1][1], [1][2]
+#  [ 7, 8, 9 ]  -->  [2][0], [2][1], [2][2]
 # ]
 # Example Output:  [1, 2, 4, 7, 5, 3, 6, 8, 9]
 
 def diagonal_traversal(matrix)
-  raise NotImplementedError, "This method has not yet been implemented"
+  return [] if matrix[0].empty?
+
+  row_length = matrix.length
+  col_length = matrix[0].length
+  diagonals = []
+
+  row_length.times do |i|
+    col_length.times do |j|
+      diagonal = i + j
+
+      if diagonals[diagonal].nil?
+        diagonals[diagonal] = Array.new
+      end
+
+      if diagonal.even?
+        diagonals[diagonal].unshift(matrix[i][j])
+      else
+        diagonals[diagonal] << matrix[i][j]
+      end
+    end
+  end
+
+  return diagonals.flatten
 end
