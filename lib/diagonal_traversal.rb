7813fbfa8810d181b8aafed723bf5dfda26cd5a9
Ringo Alcock <ringo.alcock@gmail.com>
2020-12-18 18:54:09 -0800

    define diagonal_traversal method and flip helper method

--- lib/diagonal_traversal.rb
+++ lib/diagonal_traversal.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 # Input:  A matrix of M x N elements (M rows, N columns), 
 # Return value:  All elements of the matrix in diagonal order.
 #  Example input:
@@ -9,6 +9,46 @@
 # ]
 # Example Output:  [1, 2, 4, 7, 5, 3, 6, 8, 9]
 
+# time complexity: O(n)
+# space complexity: O(n)
 def diagonal_traversal(matrix)
-  raise NotImplementedError, "This method has not yet been implemented"
+  row = 0
+  column = 0
+  diagonal = []
+  row_adjust = -1
+  column_adjust = 1
+
+  until diagonal.length == matrix.flatten.length
+    diagonal << matrix[row][column]
+    row += row_adjust
+    column += column_adjust
+
+    flip = flip(row, column, matrix.length, matrix[0].length)
+    if flip
+      row = flip[:row]
+      column = flip[:column]
+      row_adjust *= -1
+      column_adjust *= -1
+    end
+  end
+
+  return diagonal
+end
+
+def flip(row, column, row_count, column_count)
+  if row == row_count
+    row -= 1
+    column += 2
+  elsif column == column_count
+    column -= 1
+    row += 2
+  elsif row < 0
+    row += 1
+  elsif column < 0
+    column += 1
+  else
+    return false
+  end
+
+  return { row: row, column: column }
 end
